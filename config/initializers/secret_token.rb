b1c49cee809ef7b8937983d46b49846ffd42182d
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2013-07-18 05:34:22 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Magaz::Application.config.secret_key_base = '8c3d20d358444707bd4884845baa90a526bcd878e1ef6269d903eea2cec5e0480358fe9835c4ac8f2e996c405280b30595abf2bc4a080c4ec23291016857a59b'
