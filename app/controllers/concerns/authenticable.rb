7e72b6484951f5c37e723bf5005659c8808642ca
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2013-07-31 01:12:27 +0300

    authenticable controller consern

---
+++ app/controllers/concerns/authenticable.rb
@@ -0,0 +1,15 @@
+module Authenticable extend ActiveSupport::Concern
+  included do
+    before_action :authentication_required
+  end
+
+  protected
+
+  def authentication_required
+    redirect_to new_session_path unless current_shop
+  end
+
+  def current_shop
+    @current_shop ||= Shop.find_by_id(session[:shop_id])
+  end
+end
