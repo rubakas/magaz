86e75a8a60964255fb9aba5111b886f7c4acfafa
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2013-08-12 02:16:55 +0300

    use rails permitted params naming convention

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,7 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    @shop = Shop.new allowed_params
+    @shop = Shop.new permitted_params
     if @shop.save
       session[:shop_id]=@shop.id
       redirect_to admin_dashboard_path
@@ -16,14 +16,14 @@ class RegistrationsController < ApplicationController
   end
 
   def validate
-    @shop = Shop.new allowed_params
+    @shop = Shop.new permitted_params
     @shop.valid?
     render json: @shop.errors.to_json
   end
 
   private
   
-  def allowed_params
+  def permitted_params
     params.require(:shop).permit(:name,:email,:password)
   end
 end
