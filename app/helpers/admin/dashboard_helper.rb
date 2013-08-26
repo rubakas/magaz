251c11838037ca9349df929d2b22e656b0219f9a
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2013-08-26 15:22:33 +0300

    highlight sidebar links

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -1,2 +1,24 @@
 module Admin::DashboardHelper
+  # def li_link_to(link_label, link_path, conditions)
+  #   style_class = 'active' if current_page?(conditions)
+  #   content_tag :li, class: style_class do
+  #     link_to link_label, link_path
+  #   end
+  # end
+
+  def li_link_to(link_path: link_path, 
+    active_conditions: active_conditions,
+    link_icon_class: link_icon_class,
+    link_label: link_label)
+
+    active_class = 'active' if current_page?(active_conditions)
+
+    render partial: 'admin/shared/li_link_to', 
+      locals: {
+        link_path: link_path,
+        active_class: active_class,
+        link_icon_class: link_icon_class,
+        link_label: link_label
+      }
+  end
 end
