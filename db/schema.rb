ceb76884eff7036bdc56ab0d43481aeadf15e122
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2013-08-26 17:36:39 +0300

    table to associate products and collections

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130826114821) do
+ActiveRecord::Schema.define(version: 20130826143517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20130826114821) do
     t.integer  "shop_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "collections_products", force: true do |t|
+    t.integer "collection_id"
+    t.integer "product_id"
   end
 
   create_table "products", force: true do |t|
