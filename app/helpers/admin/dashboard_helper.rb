4e241dc7a5b0706932734562df5340e8a02809b1
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2013-08-26 15:43:26 +0300

    another reformatting

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -6,19 +6,19 @@ module Admin::DashboardHelper
   #   end
   # end
 
-  def li_link_to(link_path: link_path, 
-    active_conditions: active_conditions,
-    link_icon_class: link_icon_class,
-    link_label: link_label)
+  def li_link_to(path: link_path, 
+    icon_class: link_icon_class,
+    label: link_label,
+    active_conditions: active_conditions)
 
     active_class = 'active' if current_page?(active_conditions)
 
     render partial: 'admin/shared/li_link_to', 
       locals: {
-        link_path: link_path,
-        active_class: active_class,
-        link_icon_class: link_icon_class,
-        link_label: link_label
+        path: path,
+        icon_class: icon_class,
+        label: label,
+        active_class: active_class
       }
   end
 end
