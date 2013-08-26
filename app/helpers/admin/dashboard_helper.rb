c7ac6193fd1fcdef0e80aae2b9afd88d35bffb1e
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2013-08-26 15:59:38 +0300

    i do not need helper just stick with partial

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -1,24 +1,2 @@
 module Admin::DashboardHelper
-  # def li_link_to(link_label, link_path, conditions)
-  #   style_class = 'active' if current_page?(conditions)
-  #   content_tag :li, class: style_class do
-  #     link_to link_label, link_path
-  #   end
-  # end
-
-  def li_link_to(path: link_path, 
-    icon_class: link_icon_class,
-    label: link_label,
-    active_conditions: active_conditions)
-
-    active_class = 'active' if current_page?(active_conditions)
-
-    render partial: 'admin/shared/li_link_to', 
-      locals: {
-        path: path,
-        icon_class: icon_class,
-        label: label,
-        active_class: active_class
-      }
-  end
 end
