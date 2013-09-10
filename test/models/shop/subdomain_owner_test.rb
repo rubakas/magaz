a8eebc9ea86b925d6ecfef7909908fc9eaf50e77
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2013-09-10 16:13:30 +0300

    more pending tests added

--- test/models/shop/subdomain_owner_test.rb
+++ test/models/shop/subdomain_owner_test.rb
@@ -6,4 +6,20 @@ class Shop::SubdomainOwnerTest < ActiveSupport::TestCase
     assert shop.valid?
     assert_equal shop.subdomain, 'uniq-name'
   end
+
+  test 'subdomain format' do
+    skip
+  end
+
+  test 'subdomain format - characters stripped' do
+    skip
+  end
+
+  test 'subdomain uniqueness' do
+    skip
+  end
+
+  test 'subdomain uniqueness - subdomain name is generated if needed' do
+    skip
+  end
 end
