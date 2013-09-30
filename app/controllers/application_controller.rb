1d778751e4518dfb40190ca5b52a59c5c49927a6
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2013-09-30 14:11:11 +0300

    protect helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
+  protected
+  
   def current_shop
     @current_shop ||= Shop.find_by_subdomain(request.subdomain)
   end
