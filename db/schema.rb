0e0b10d48e8079c682b2ec8e4fa2c1125b54903e
Markell Pogosov <mpogosov@heliostech.hk>
2013-11-07 21:38:42 +0200

    added scaffold pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131101032033) do
+ActiveRecord::Schema.define(version: 20131107193139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admin_blogs", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "admin_pages", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+    t.string   "description"
+  end
+
+  create_table "blogs", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "collections", force: true do |t|
     t.string   "name"
