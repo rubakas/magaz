8d5329c1f9bee7703129ced3e68fb426ae33f31d
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2013-11-08 07:37:41 +0200

    annotations added

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: pages
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :string(255)
+#  shop_id     :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Page < ActiveRecord::Base
   belongs_to :shop
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,15 +2,13 @@
 #
 # Table name: products
 #
-#  id           :integer          not null, primary key
-#  name         :string(255)
-#  description  :text
-#  created_at   :datetime
-#  updated_at   :datetime
-#  shop_id      :integer
-#  product_type :string(255)
-#  sku          :string(255)
-#  price        :decimal(38, 2)
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#  shop_id     :integer
+#  price       :decimal(38, 2)
 #
 
 class Product < ActiveRecord::Base
