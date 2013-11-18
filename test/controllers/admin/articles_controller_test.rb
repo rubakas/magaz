328fe8be1145527c77c83da3f3f86703357aef0f
Markell Pogosov <szerencsbtlen404@gmail.com>
2013-11-18 11:43:22 +0200

    added some controllers tests

--- test/controllers/admin/articles_controller_test.rb
+++ test/controllers/admin/articles_controller_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class Admin::ArticlesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    session_for_shop shops(:shop_1)
+    @article = articles(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:articles)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create article" do
+    assert_difference('Article.count') do
+      post :create, article: { content: @article.content, title: 'New uniq name' }
+    end
+
+    assert_redirected_to admin_article_path(assigns(:article))
+  end
+
+  test "should show article" do
+    get :show, id: @article
+    assert_response :success
+  end
+
+  test "should update article" do
+    patch :update, id: @article, article: { content: @article.content, title: @article.title }
+    assert_response :success
+  end
+
+  test "should destroy article" do
+    assert_difference('Article.count', -1) do
+      delete :destroy, id: @article
+    end
+
+    assert_redirected_to admin_articles_path
+  end
 end
--- test/controllers/admin/blogs_controller_test.rb
+++ test/controllers/admin/blogs_controller_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class Admin::BlogsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    session_for_shop shops(:shop_1)
+    @blog = blogs(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:blogs)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create blog" do
+    assert_difference('Blog.count') do
+      post :create, blog: { title: 'New uniq name' }
+    end
+
+    assert_redirected_to admin_blog_path(assigns(:blog))
+  end
+
+  test "should show blog" do
+    get :show, id: @blog
+    assert_response :success
+  end
+
+  test "should update blog" do
+    patch :update, id: @blog, blog: { title: @blog.title }
+    assert_response :success
+  end
+
+  test "should destroy blog" do
+    assert_difference('Blog.count', -1) do
+      delete :destroy, id: @blog
+    end
+
+    assert_redirected_to admin_blogs_path
+  end
 end
