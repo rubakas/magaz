412c9928538c18ea93704c031ec9bf61ab92f0c0
Markell Pogosov <szerencsbtlen404@gmail.com>
2013-11-22 16:41:41 +0200

    added some integration tests

---
+++ test/integration/admin/articles_stories_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+class Admin::ArticlesStoriesTest < ActionDispatch::IntegrationTest
+  setup do
+    login_as shop_name: 'Example',
+      email: 'admin@example.com',
+      password: 'password'
+    click_link 'Blog Posts'
+  end
+
+  test "blog posts list" do
+    assert page.has_content? 'Blog Posts'
+  end
+
+  test "create blog post" do
+    click_link 'Add Blog Post'
+    fill_in 'Title', with: 'Some Uniq Blog Post'
+    fill_in 'Content', with: ''
+    click_button 'Create Article'
+    assert page.has_content? 'Blog Post was successfully created'
+  end
+
+  test "create blog post failure" do
+    click_link 'Add Blog Post'
+    fill_in 'Title', with: ''
+    fill_in 'Content', with: ''
+    click_button 'Create Article'
+    assert page.has_content? '1 error prohibited this article from being saved'
+  end
+
+  test "edit blog post" do
+    click_link(Article.first.title, match: :first)
+    fill_in 'Title', with: 'Updated Blog Post'
+    fill_in 'Content', with: 'Updated Content'
+    click_button 'Update Article'
+    assert page.has_content? 'Blog Post was successfully updated'
+  end
+
+  test "delete blog post" do
+    assert page.has_content? 'Destroy'
+    click_link('Destroy', match: :first)
+    refute page.has_content? "You have no blog posts yet, let's create one!"
+  end
+end
