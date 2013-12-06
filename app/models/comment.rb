56f545cb6e6d841eb4b961aa9b27f169910e5132
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2013-12-06 10:37:24 +0200

    annotations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id          :integer          not null, primary key
+#  author      :string(255)
+#  body        :text
+#  email       :string(255)
+#  blog_id     :integer
+#  articles_id :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Comment < ActiveRecord::Base
   belongs_to :blog
   belongs_to :article
