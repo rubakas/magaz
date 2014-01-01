83ed7f578b55d1cc8c2fb76390746186c7f80987
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2014-01-01 20:15:48 +0200

    more controllers rewritten with IR

--- app/controllers/admin/collections_controller.rb
+++ app/controllers/admin/collections_controller.rb
@@ -1,55 +1,29 @@
 class Admin::CollectionsController < ApplicationController
   include Authenticable
-  before_action :set_collection, only: [:show, :update, :destroy]
+  inherit_resources
+  actions :all, :except => [:edit]
 
-  # GET /admin/collections
-  def index
-    @collections = current_shop.collections.page(params[:page])
-  end
-
-  # GET /admin/collections/new
-  def new
-    @collection = current_shop.collections.build
-  end
-
-  # GET /admin/collections/1
-  def show
-  end
-
-  # POST /admin/collections
-  def create
-    @collection = current_shop.collections.build(collection_params)
-
-    if @collection.save
-      redirect_to [:admin, @collection], notice: 'Collection was successfully created.'
-    else
-      render action: 'new'
-    end
-  end
-
-  # PATCH/PUT /admin/collections/1
   def update
-    if @collection.update(collection_params)
-      redirect_to [:admin, @collection], notice: 'Collection was successfully updated.'
-    else
-      render action: 'show'
+    update! do |success, failure|
+      failure.html { render :show }
     end
   end
 
-  # DELETE /admin/collections/1
-  def destroy
-    @collection.destroy
-    redirect_to admin_collections_url
+
+  protected
+
+  def begin_of_association_chain
+    current_shop
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_collection
-      @collection = current_shop.collections.find(params[:id])
-    end
+  def collection
+    @collections ||= end_of_association_chain.page(params[:page])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def collection_params
-      params.require(:collection).permit(:name, :description)
-    end
+  #TODO collection_ids are not guaranteed to belong to this shop!!!
+  # https://github.com/josevalim/inherited_resources#strong-parameters
+  def permitted_params
+    { collection: 
+        params.fetch(:collection, {}).permit(:name, :description) }
+  end
 end
