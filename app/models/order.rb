c6eddeec8ec123ba977c92049785d878b798eab0
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2014-03-18 12:32:17 +0200

    shopping cart refactoring

--- app/models/order.rb
+++ app/models/order.rb
@@ -22,46 +22,5 @@ class Order < ActiveRecord::Base
   has_many :line_items
   belongs_to :shop
 
-  delegate :each, :empty?, to: :line_items
-
-  def add_product(product:, quantity: 1)
-    raise "Bad quantity" if quantity < 1
-    existing_line_item = 
-      line_items.find { |li| li.product_id == product.id }
-    if existing_line_item
-      existing_line_item.quantity += quantity
-    else
-      new_li_attrs = LineItem.attribute_names.map(&:to_sym) - [:id, :shop_id]
-      copied_attrs = product.
-        attributes.
-        merge({product: product, product_id: product.id, quantity: quantity}).
-        select{|k, v| new_li_attrs.include?(k.to_sym) }
-      line_items.create(copied_attrs)
-    end
-  end
-
-  def items
-    line_items
-  end
-
-  def item_count
-    line_items.map.sum(&:quantity)
-  end
-
-  def total_price
-    line_items.map.sum(&:line_price)
-  end
-
-  def total_weight
-    line_items.map.sum(&:line_weight)
-  end
-
-  #TODO extract service?
-  def update_with_hash(id_qty_hash)
-    line_items.clear
-    id_qty_hash.each do |k,v|
-      add_product(product: Product.find(k), quantity: v.to_i)
-    end
-    line_items
-  end
+  include ShoppingCart
 end
