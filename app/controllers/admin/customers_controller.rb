2a4065c0a1ae4e3f448ac0ed8a423504f28c6827
Markell Pogosov <szerencsbtlen404@gmail.com>
2014-04-24 00:19:01 +0300

    add customer's views

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,24 @@
+class Admin::CustomersController < ApplicationController
+  include Authenticable
+  inherit_resources
+  actions :all, :except => [:edit]
+
+  def update
+    update! do |success, failure|
+      failure.html { render :show }
+    end
+  end
+
+  protected
+
+  def collection
+    @customers ||= end_of_association_chain.page(params[:page])
+  end
+
+  #TODO:  collection_ids are not guaranteed to belong to this shop!!!
+  # https://github.com/josevalim/inherited_resources#strong-parameters
+  def permitted_params
+    { customer:
+        params.fetch(:customer, {}).permit(:first_name, :last_name, :email) }
+  end
+end
