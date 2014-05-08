543f61778a30dd1e7afb0e7eeed56182781ce688
Markell Pogosov <szerencsbtlen404@gmail.com>
2014-05-09 02:32:03 +0300

    some checkouts administration

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,7 @@ Rails.application.routes.draw do
       root 'dashboard#index'
       resources :articles, except: [:edit]
       resources :blogs, except: [:edit]
+      resources :checkouts, except: [:create, :edit, :new]
       resources :collections, except: [:edit]
       resources :comments, except: [:edit]
       resources :customers, except: [:edit]
