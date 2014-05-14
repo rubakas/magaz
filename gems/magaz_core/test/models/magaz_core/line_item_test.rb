4e56413d2850427402e32134551274d3624e327b
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2014-05-14 15:00:59 +0300

    fix for nil price case

--- gems/magaz_core/test/models/magaz_core/line_item_test.rb
+++ gems/magaz_core/test/models/magaz_core/line_item_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 module MagazCore
   class LineItemTest < ActiveSupport::TestCase
+    #TODO add test for line_price method nil price case
+    
     # test "the truth" do
     #   assert true
     # end
