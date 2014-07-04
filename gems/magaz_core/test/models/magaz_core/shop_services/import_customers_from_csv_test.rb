ee843f7dd73d5c1a1da11400a259f0142a6d6057
Markell Pogosov <szerencsbtlen404@gmail.com>
2014-07-04 15:58:43 +0300

    add fixture file

--- gems/magaz_core/test/models/magaz_core/shop_services/import_customers_from_csv_test.rb
+++ gems/magaz_core/test/models/magaz_core/shop_services/import_customers_from_csv_test.rb
@@ -7,17 +7,7 @@ module MagazCore
     end
 
     test 'import customers' do
-
-      csv_rows = <<-eos
-id,first_name,last_name,email
-30,FirstName1,LastName1,name1@example.com
-20,FirstName2,LastName2,name2@example.com
-eos
-
-      file = Tempfile.new('new_customers.csv')
-      file.write(csv_rows)
-      file.rewind
-
+      file = File.expand_path('./../../../../fixtures/files/customers.csv', __FILE__)
       assert_difference "Customer.count", 2 do
         MagazCore::ShopServices::ImportCustomersFromCsv.call(shop_id: @shop.id, csv_file: Rack::Test::UploadedFile.new(file, 'text/csv'))
       end
