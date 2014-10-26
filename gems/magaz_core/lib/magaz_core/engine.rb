2c4e76b9dc79472b4ccf430ab8bab4d8dcccee6b
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2014-10-26 16:51:46 +0200

    refactored core engine initializer

--- gems/magaz_core/lib/magaz_core/engine.rb
+++ gems/magaz_core/lib/magaz_core/engine.rb
@@ -2,6 +2,18 @@ module MagazCore
   class Engine < ::Rails::Engine
     isolate_namespace MagazCore
 
+    def _include_dependencies_at(dir)
+      Dir.glob(app.root + "#{dir}/**/*.rb").each {|c| require_dependency(c)}
+    end
+
+    def for_wrapper_app_config_include_dir(dir)
+      unless app.root.to_s.match root.to_s
+        config.to_prepare do
+          _include_dependencies_at dir
+        end
+      end
+    end
+
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
@@ -11,23 +23,11 @@ module MagazCore
     end
 
     initializer :append_uploaders do |app|
-      unless app.root.to_s.match root.to_s
-        config.to_prepare do
-          Dir.glob(app.root + "app/uploaders/**/*_uploader*.rb").each do |c|
-            require_dependency(c)
-          end
-        end
-      end
+      for_wrapper_app_config_include_dir "app/uploaders"
     end
 
     initializer :append_constraints do |app|
-      unless app.root.to_s.match root.to_s
-        config.to_prepare do
-          Dir.glob(app.root + "app/constraints/**/*.rb").each do |c|
-            require_dependency(c)
-          end
-        end
-      end
+      for_wrapper_app_config_include_dir "app/uploaders"
     end
     
   end
