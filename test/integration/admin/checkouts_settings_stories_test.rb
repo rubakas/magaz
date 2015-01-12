9f72c2a7a0e0b14f742ff4fe364e34539b775e63
Griminy <bozya003@gmail.com>
2015-01-12 14:55:38 +0200

    i hope it's last one fix of checkout and payments settings with tests 2

--- test/integration/admin/checkouts_settings_stories_test.rb
+++ test/integration/admin/checkouts_settings_stories_test.rb
@@ -14,10 +14,10 @@ class Admin::CheckoutsSettingsStoriesTest < ActionDispatch::IntegrationTest
   test "update checkouts settings" do
     assert_not page.has_css?('.js-checkouts_hidden_field_multipass')
     assert page.has_css?('.js-checkouts_hidden_field_auto_fulfill') 
-    find('#shop_account_type_choice').find(:xpath, 'option[1]').select_option
-    find('#shop_abandoned_checkout_time_delay').find(:xpath, 'option[1]').select_option
-    find('#shop_email_marketing_choice').find(:xpath, 'option[1]').select_option
-    find('#shop_after_order_paid').find(:xpath, 'option[2]').select_option
+    find('#shop_account_type_choice').first( 'option').select_option
+    find('#shop_abandoned_checkout_time_delay').first('option').select_option
+    find('#shop_email_marketing_choice').first('option').select_option
+    find('#shop_after_order_paid').first('option').select_option
     check('shop_after_order_fulfilled_and_paid')    
     click_button 'Save'
     assert page.has_content? 'Shop was successfully updated.'
