f8055bbf0412ca56282b45a5d4d33874f28e7583
Markell Pogosov <szerencsbtlen404@gmail.com>
2015-01-15 03:16:50 +0200

    rewrited controller tests

--- test/controllers/admin/shipping_rates_controller_test.rb
+++ test/controllers/admin/shipping_rates_controller_test.rb
@@ -4,37 +4,37 @@ class Admin::ShippingRatesControllerTest < ActionController::TestCase
   setup do
     @shop = create(:shop, subdomain: 'example')
     session_for_shop @shop
-    @country = create(:country, shop: @shop)
-    @shipping_rate = create(:shipping_rate, country: @country)
+    @shipping_country = create(:shipping_country, shop: @shop)
+    @shipping_rate = create(:shipping_rate, shipping_country: @shipping_country)
   end
 
   test "should get new" do
-    get :new, :country_id => @country.id
+    get :new, :shipping_country_id => @shipping_country.id
     assert_response :success
   end
 
   test "should create shipping_rate" do
     assert_difference('MagazCore::ShippingRate.count') do
-      post :create, { :country_id => @country.id, shipping_rate: { name: 'Very Unique Name', shipping_price: '12' } }
+      post :create, { :shipping_country_id => @shipping_country.id, shipping_rate: { name: 'Very Unique Name', shipping_price: '12' } }
     end
     assert_response :success
   end
 
   test "should show shipping_rate" do
-    get :show, :country_id => @country.id, id: @shipping_rate
+    get :show, :shipping_country_id => @shipping_country.id, id: @shipping_rate
     assert_response :success
   end
 
   test "should update shipping_rate" do
     patch :update,
-      { :country_id => @country.id, id: @shipping_rate.id,
+      { :shipping_country_id => @shipping_country.id, id: @shipping_rate.id,
         shipping_rate: { name: @shipping_rate.name } }
-    assert_redirected_to admin_country_shipping_rate_path(@country, @shipping_rate)
+    assert_redirected_to admin_shipping_country_shipping_rate_path(@shipping_country, @shipping_rate)
   end
 
   test "should not update shipping_rate" do
     patch :update,
-      { :country_id => @country.id, id: @shipping_rate.id,
+      { :shipping_country_id => @shipping_country.id, id: @shipping_rate.id,
         shipping_rate: { shipping_price: '', name: '' } }
     assert_template :show
     assert_response :success
@@ -42,23 +42,23 @@ class Admin::ShippingRatesControllerTest < ActionController::TestCase
 
   test "should destroy shipping_rate" do
     assert_difference('MagazCore::ShippingRate.count', -1) do
-      delete :destroy, :country_id => @country.id, id: @shipping_rate.id
+      delete :destroy, :shipping_country_id => @shipping_country.id, id: @shipping_rate.id
     end
 
-    assert_redirected_to admin_country_path(@country)
+    assert_redirected_to admin_shipping_country_path(@shipping_country)
   end
 
   test "criteria validation success" do
     patch :update,
-      { :country_id => @country.id, id: @shipping_rate.id,
+      { :shipping_country_id => @shipping_country.id, id: @shipping_rate.id,
         shipping_rate: { shipping_price: @shipping_rate.shipping_price, name: @shipping_rate.name,
                           criteria: "price", price_from: 10, price_to: 20} }
-    assert_redirected_to admin_country_shipping_rate_path(@country, @shipping_rate)
+    assert_redirected_to admin_shipping_country_shipping_rate_path(@shipping_country, @shipping_rate)
   end
 
   test "criteria validation failure" do
     patch :update,
-      { :country_id => @country.id, id: @shipping_rate.id,
+      { :shiping_country_id => @shipping_country.id, id: @shipping_rate.id,
         shipping_rate: { shipping_price: @shipping_rate.shipping_price, name: @shipping_rate.name,
                           criteria: "weight", price_from: 10, price_to: 20} }
     assert_template :show
