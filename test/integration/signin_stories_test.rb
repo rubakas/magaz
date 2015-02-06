16e467f6c5e823b405d5b2684db4a59ecc47745b
Griminy <bozya003@gmail.com>
2015-02-06 18:44:19 +0200

    tests for user sign in falture

--- test/integration/signin_stories_test.rb
+++ test/integration/signin_stories_test.rb
@@ -3,10 +3,48 @@ require 'test_helper'
 class SigninStoriesTest < ActionDispatch::IntegrationTest
   setup do
     use_host HOSTNAME
+    @shop2 = create(:shop, name: 'example2', subdomain: 'example2')
+    @user2 = create(:user, shop: @shop2, first_name: 'First2', last_name: 'Last2', email: 'email2@mail.com', password: 'password2')
   end
 
   test "sign in redirect" do
     visit '/session'
+    assert page.has_content?('Sign in')
+  end
+
+  test "wrong user" do
+    visit '/'
+    assert page.has_content?('Sign in')
+
+    click_link 'Sign in'
+
+    fill_in 'Your shop name', with: @shop.name
+    fill_in 'Email address', with: @user2.email
+    fill_in 'Password', with: 'password2'
+
+    use_subdomain @shop.subdomain
+
+    click_button 'Sign in'
+
+    assert page.has_content?('Sign in')
+
+    fill_in 'Email address', with: @user.email
+    fill_in 'Password', with: 'password'
+
+    assert page.has_content?('Dashboard')
+  end
+
+  test "wrong user for shop" do
+    use_subdomain @shop.subdomain
+    visit '/admin'
+    click_link 'Sign out' if page.has_content?('Sign out')
+
+    visit '/admin'
+    assert page.has_content?('Sign in')
+
+    fill_in 'Email address', with: @user2.email
+    fill_in 'Password', with: 'password2'
+
     assert page.has_content?('Sign in')
   end
 
