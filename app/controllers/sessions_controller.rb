90b74b097d687cd93360fb48f5ecdf2bbc196c4e
Griminy <bozya003@gmail.com>
2015-02-10 16:53:22 +0200

    fix admin session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
       !@shop.users.find_by(id: @user.id) == nil)
       session[:user_id] = @user.id
       redirect_to admin_root_url(host: HOSTNAME, subdomain: @shop.subdomain)
-    else
+    elsif
       if @shop
         flash[:alert] = I18n.t('sessions.create.wrong_password')
       else
