71b4b8df08e7b5ecd6e93fb2352d2c197406d975
Griminy <bozya003@gmail.com>
2015-02-12 18:58:17 +0200

    fix create service

--- gems/magaz_core/app/models/magaz_core/shop_services/create.rb
+++ gems/magaz_core/app/models/magaz_core/shop_services/create.rb
@@ -9,6 +9,7 @@ module MagazCore
 
         MagazCore::Shop.connection.transaction do
           begin
+            _create_user!(user_params: user_params, shop: @shop)
             _save_shop_record!(shop: @shop, params: shop_params, user_params: user_params)
             _install_default_theme(shop: @shop)
             _create_default_blogs_and_posts!(shop: @shop)
@@ -24,9 +25,13 @@ module MagazCore
 
       private
 
+      def _create_user!(user_params:, shop:)
+        MagazCore::UserServices::CreateUser.call(user_params: user_params,
+                                                 shop: shop)
+      end
+
       def _save_shop_record!(shop:, params:, user_params:)
         shop.attributes = params
-        _create_user(user_params: user_params, shop: shop)
         shop.save!
       end
 
@@ -35,11 +40,6 @@ module MagazCore
         default_theme = MagazCore::Theme.sources.first || fail(ActiveRecord::RecordNotFound)
         MagazCore::ThemeServices::Install
           .call(shop_id: shop.id, source_theme_id: default_theme.id)
-      end
-
-      def _create_user(user_params:, shop:)
-        MagazCore::UserServices::CreateUser.call(user_params: user_params,
-                                                 shop: shop)
       end
 
       def _create_default_blogs_and_posts!(shop:)
