ed5c8c7d23331b381d831d3774d5f0e722351da3
Markell Pogosov <szerencsbtlen404@gmail.com>
2015-02-27 18:28:56 +0200

    minor fix

--- gems/magaz_core/app/models/magaz_core/shop_services/create_invite.rb
+++ gems/magaz_core/app/models/magaz_core/shop_services/create_invite.rb
@@ -14,7 +14,7 @@ module MagazCore
             _valid_email(email: email, shop: shop)
             _create_user_with_email_and_token!(user: @user, email: email, shop_id: shop.id)
             _send_mail_invite(user: @user, email: email, host: host )
-          rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordInvalid
+          rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordInvalid, ArgumentError
             raise ActiveRecord::Rollback
           end
         end
@@ -32,13 +32,13 @@ module MagazCore
         valid_email = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
         email.present? &&
           (email =~ valid_email) &&
-          shop.users.find_by(email: email).nil? || fail(ActiveRecord::RecordNotFound)
+          shop.users.find_by(email: email).nil? || fail(ArgumentError)
       end
 
       def _send_mail_invite(user:, email:, host:)
         MagazCore::UserMailer.invite_new_user(user,
                               admin_user_url(user, host: host, invite_token: user.invite_token),
-                              host).deliver_now || fail(ActiveRecord::RecordNotFound)
+                              host).deliver_now || fail(ArgumentError)
       end
     end
   end
