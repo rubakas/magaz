7f5124221e8100928d781966e9438584cb6bd856
Griminy <bozya003@gmail.com>
2015-05-08 18:42:12 +0300

    fix initializer

--- config/initializers/webhooks.rb
+++ config/initializers/webhooks.rb
@@ -1,24 +1,25 @@
 require 'json'
+require 'net/http'
+require 'net/https'
+require 'uri'
 
 ActiveSupport::Notifications.subscribe 'events' do |name, data|
-  # puts "*******************************"
-  # Rails.logger.debug(["notification:", shop_id, subject_id].join(" "))
-  # puts "*******************************"
   shop = MagazCore::Shop.find(data[:event][:shop_id])
   webhook = shop.webhooks.find_by(topic: "Product creation")
   unless webhook == nil
     if webhook.format == "XML"
-      some = data.to_xml
-      puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-      puts some
-      puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+      mail = data.to_xml
+      format = 'xml'
     else
-      some = data.to_json
-      puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-      puts some
-      puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+      mail = data.to_json
+      format = 'json'
     end
-  else
-    puts "*****************************************************"
+    uri = URI.parse("#{webhook.address}")
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+    req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/#{format}'})
+    req.body = "[ #{mail} ]"
+    res = https.request(req)
+    puts "Response #{res.code} #{res.message}: #{res.body}"
   end
 end
