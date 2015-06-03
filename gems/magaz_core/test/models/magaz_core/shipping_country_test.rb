42001f93b875f5e192f93cf6bfaaa9e3cc0a3266
Markell Pogosov <szerencsbtlen404@gmail.com>
2015-06-03 13:32:26 +0300

    country_info test

--- gems/magaz_core/test/models/magaz_core/shipping_country_test.rb
+++ gems/magaz_core/test/models/magaz_core/shipping_country_test.rb
@@ -18,5 +18,9 @@ module MagazCore
       assert_not shipping_country.valid?
       assert_equal ["is not a number"], shipping_country.errors.messages[:tax]
     end
+
+    test "should return country info" do
+      assert_equal @shipping_country.country_info['iso_2'], @shipping_country.name
+    end
   end
 end
