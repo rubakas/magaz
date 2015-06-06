08878a9216270d86e93bf13ed58c849ab104d5c0
Griminy <bozya003@gmail.com>
2015-06-06 14:40:58 +0300

    fix tests for event_webhook_runner

--- gems/magaz_core/test/services/magaz_core/shop_services/event_webhook_runner_test.rb
+++ gems/magaz_core/test/services/magaz_core/shop_services/event_webhook_runner_test.rb
@@ -1,4 +1,6 @@
 require 'test_helper'
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
 
 module MagazCore
   class ShopServices::EventWebhookRunnerTest < ActiveSupport::TestCase
@@ -10,24 +12,26 @@ module MagazCore
 
       @webhook3 = create(:webhook, shop: @shop)
       @webhook3.update_attributes!(topic: "Product creation")
-      @webhook4 = create(:webhook, shop: @shop)
-      @webhook4.update_attributes!(topic: "Product creation")
 
       @product = create(:product, shop: @shop)
       @event = create(:event, shop: @shop, subject: @product)
     end
 
-    test 'should not throw error' do
+    test 'should not throw error and schedule 2 webhooks' do
       assert_nothing_raised do
         service = MagazCore::ShopServices::EventWebhookRunner.call(event: @event,
-                                                                   webhook: "Product update")
+                                                                   webhook: @webhook1.topic)
+        assert_equal 2, MagazCore::WebhookWorker.jobs.size
       end
     end
 
     test 'should schedule webhook' do
-      service = MagazCore::ShopServices::EventWebhookRunner.call(event: @event,
-                                                                 webhook: @webhook1)
-      assert_send([@event, :to_xml])
+      Sidekiq::Worker.clear_all
+      mail = @event.to_xml
+      format = @webhook1.format.downcase
+      address = @webhook1.address
+      MagazCore::WebhookWorker.perform_async(mail, address, format)
+      assert_equal 1, MagazCore::WebhookWorker.jobs.size
     end
   end
 end
