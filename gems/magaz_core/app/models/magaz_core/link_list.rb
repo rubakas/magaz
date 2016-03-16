898343702d93a25b5301e61330e193eb503d1eb8
Griminy <bozya003@gmail.com>
2016-03-16 17:44:15 +0200

    add_link_list, tests

--- gems/magaz_core/app/models/magaz_core/link_list.rb
+++ gems/magaz_core/app/models/magaz_core/link_list.rb
@@ -19,9 +19,5 @@ module MagazCore
 
     accepts_nested_attributes_for :links
     friendly_id :handle, use: [:slugged, :scoped], scope: :shop
-
-    validates :name,
-      presence: true,
-      uniqueness: { scope: :shop_id }
   end
 end
