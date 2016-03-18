272578df2465e25aba72bbc14077de0847708434
arbagen <arbagen101@gmail.com>
2016-03-18 19:58:35 +0200

    delete links with link list

--- gems/magaz_core/test/services/magaz_core/shop_services/delete_link_list_test.rb
+++ gems/magaz_core/test/services/magaz_core/shop_services/delete_link_list_test.rb
@@ -6,6 +6,7 @@ module MagazCore
     def setup
       @shop = create(:shop, name: "shop name")
       @link_list = create(:link_list, shop: @shop)
+      @link = create(:link, link_list: @link_list)
     end
 
     test 'should delete link list with valid id' do
@@ -18,6 +19,12 @@ module MagazCore
       service = MagazCore::ShopServices::DeleteLinkList.run(id: '')
       refute service.valid?
       assert_equal 1, MagazCore::LinkList.count
+    end
+
+    test 'should delete links with link list' do
+      assert_difference('MagazCore::Link.count', -1) do
+        service = MagazCore::ShopServices::DeleteLinkList.run(id: @link_list.id)
+      end
     end
   end
 end
