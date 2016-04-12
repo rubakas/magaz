599a00f95c689452068ee54d7d28bfdaa5ba3c39
arbagen <arbagen101@gmail.com>
2016-04-12 17:54:54 +0300

    add tests, add error to en.yml

--- gems/magaz_core/app/services/magaz_core/admin_services/product/add_product.rb
+++ gems/magaz_core/app/services/magaz_core/admin_services/product/add_product.rb
@@ -29,7 +29,7 @@ class MagazCore::AdminServices::Product::AddProduct < ActiveInteraction::Base
   private
 
   def name_uniqueness
-    errors.add(:base, I18n.t('default.services.add_product.title_not_unique')) unless name_unique?
+    errors.add(:base, I18n.t('default.services.add_product.name_not_unique')) unless name_unique?
   end
 
   def name_unique?
