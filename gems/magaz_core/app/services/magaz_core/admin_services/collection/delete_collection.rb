a5eccecce7ed0808787215b6bef1175f554547cb
arbagen <arbagen101@gmail.com>
2016-04-25 18:08:52 +0300

    delete collection

--- gems/magaz_core/app/services/magaz_core/admin_services/collection/delete_collection.rb
+++ gems/magaz_core/app/services/magaz_core/admin_services/collection/delete_collection.rb
@@ -1,10 +1,11 @@
 class MagazCore::AdminServices::Collection::DeleteCollection < ActiveInteraction::Base
 
-  integer :id
+  integer :shop_id
+  string :id
 
-  validates :id, presence: true
+  validates :id, :shop_id, presence: true
 
   def execute
-    MagazCore::Collection.friendly.find(id).destroy
+    MagazCore::Shop.find(shop_id).collections.friendly.find(id).destroy
   end
 end
--- gems/magaz_core/test/services/magaz_core/admin_services/collection/delete_collection_test.rb
+++ gems/magaz_core/test/services/magaz_core/admin_services/collection/delete_collection_test.rb
@@ -8,14 +8,18 @@ class MagazCore::AdminServices::Collection::DeleteCollectionTest < ActiveSupport
   end
 
   test 'should delete collection with valid id' do
-    service = MagazCore::AdminServices::Collection::DeleteCollection.run(id: @collection.id)
+    assert MagazCore::Collection.find_by_id(@collection.id)
+    service = MagazCore::AdminServices::Collection::DeleteCollection
+                .run(id: @collection.id.to_s, shop_id: @shop.id)
     assert service.valid?
+    refute MagazCore::Collection.find_by_id(@collection.id)
     assert_equal 0, MagazCore::Collection.count
   end
 
   test 'should not delet collection with blank id' do
-    service = MagazCore::AdminServices::Collection::DeleteCollection.run(id: '')
+    service = MagazCore::AdminServices::Collection::DeleteCollection.run(id: '', shop_id: '')
     refute service.valid?
+    assert_equal 1, service.errors.count
     assert_equal 1, MagazCore::Collection.count
   end
 
