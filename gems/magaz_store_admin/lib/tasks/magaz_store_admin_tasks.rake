b2b41d84ddbcc7c76be1c37447524e63cb9a884f
Griminy <bozya003@gmail.com>
2016-05-17 13:21:47 +0300

    remove folder

--- gems/magaz_store_admin/lib/tasks/magaz_store_admin_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :magaz_store_admin do
-#   # Task goes here
-# end
--- gems/magaz_store_admin/test/helpers/customers_helper_test.rb
+++
@@ -1,6 +0,0 @@
-module MagazStoreAdmin
-  require 'test_helper'
-
-  class CustomersHelperTest < ActionView::TestCase
-  end
-end
