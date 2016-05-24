6525419a59e0ff3bcdeaf94f0f21bb17801decfd
arbagen <arbagen101@gmail.com>
2016-05-24 17:53:29 +0300

    add variables for views

--- app/controllers/theme_store/welcome_controller.rb
+++ app/controllers/theme_store/welcome_controller.rb
@@ -2,6 +2,10 @@ class ThemeStore::WelcomeController < ApplicationController
   layout 'theme_store'
   
   def authors_themes
+    @authors_themes = Array.new
+    for i in 0..5
+      @authors_themes << Theme.first
+    end
   end
 
   def demo
@@ -9,6 +13,7 @@ class ThemeStore::WelcomeController < ApplicationController
   end
 
   def homepage
+    @all_themes = Theme.all.to_a*10
   end
 
   def installing
@@ -24,9 +29,20 @@ class ThemeStore::WelcomeController < ApplicationController
   end
 
   def template_page
+    @template_themes = Array.new
+    for i in 0..5
+      @template_themes << Theme.last
+    end
   end
 
   def theme_page
+    @theme = Theme.first
+    @authors_themes = @template_themes = @styles = Array.new
+    for i in 0..5
+      @authors_themes << Theme.first
+      @styles << Theme.first
+      @template_themes << Theme.last
+    end
   end
 
 end
