c8531891336af24b0ec0bcabdbb73621a158b432
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2016-05-25 04:15:10 +0300

    update complete

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 Rails.application.config.session_store ActionDispatch::Session::CacheStore, 
   key: '_magaz_session',
-  domain: ".#{HOSTNAME}"
+  # domain: ".#{HOSTNAME}"
   domain: :all
 
