8990ef8f0eb30c1b000276befb4b210316e09924
arbagen <arbagen101@gmail.com>
2016-05-26 21:10:19 +0300

    remove validations from partner model, fix partner model tests

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,8 +1,5 @@
 class Partner < ApplicationRecord
 
   has_many :themes, dependent: :destroy
-
-  validates :name, presence: true, uniqueness: true
-  validates :website_url, presence: true, uniqueness: true
   
 end
