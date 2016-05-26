c44ab438b0c8f2a354402699496f1286a1edbc9a
arbagen <arbagen101@gmail.com>
2016-05-26 20:05:12 +0300

    remove validations from model and remove some tests

--- app/models/theme_style.rb
+++ app/models/theme_style.rb
@@ -1,8 +1,5 @@
 class ThemeStyle < ApplicationRecord
 
   belongs_to :theme
-
-  # validates :name, presence: true, uniqueness: { scope: :theme_id }
-  # validates :theme_id, presence: true
   
 end
