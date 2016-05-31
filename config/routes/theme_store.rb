dd981c66a793fcf79ff12893d6db51c0bcb3a24c
arbagen <arbagen101@gmail.com>
2016-05-31 17:01:42 +0300

    add valid variables in welcome controller, fix routes, add links in views

--- config/routes/theme_store.rb
+++ config/routes/theme_store.rb
@@ -1,12 +1,12 @@
 namespace :theme_store, path: '/' do
   root 'welcome#homepage'
-  get 'authors_themes'    => 'welcome#authors_themes'
-  get 'demo'              => 'welcome#demo'
-  get 'homepage'          => 'welcome#homepage'
-  get 'installing'        => 'welcome#installing'
-  get 'learn_more'        => 'welcome#learn_more'
-  get 'login'             => 'welcome#login'
-  get 'preview_in_store'  => 'welcome#preview_in_store'
-  get 'template_page'     => 'welcome#template_page'
-  get 'theme_page'        => 'welcome#theme_page'
+  get "authors_themes/:id"    => 'welcome#authors_themes', as: "authors_themes"
+  get "demo/:id"              => 'welcome#demo', as: "demo"
+  get 'homepage'              => 'welcome#homepage'
+  get 'installing'            => 'welcome#installing'
+  get 'learn_more'            => 'welcome#learn_more'
+  get 'login'                 => 'welcome#login'
+  get 'preview_in_store'      => 'welcome#preview_in_store'
+  get "template_page/:id"     => 'welcome#template_page', as: "template_page"
+  get "theme_page/:id"        => 'welcome#theme_page', as: "theme_page"
 end
