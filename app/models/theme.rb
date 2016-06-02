7e7c3d4d53675c9e1d52930075839d653ce37d3a
arbagen <arbagen101@gmail.com>
2016-06-02 19:38:00 +0300

    transfer scope

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -22,30 +22,17 @@ class Theme < ActiveRecord::Base
     PREMIUM = "premium".freeze
   end
 
-  module IndustryCategories
-    INDUSTRIES_LIST = ["Art & Photography", "Clothing & Fashion",
-                       "Electronics", "Food & Drink",
-                       "Health & Beauty", "Home & Garden",
-                       "Jewelry & Accessories", "Other",
-                       "Responsive", "Sports & Recreation", "Toys & Games" ].freeze
-  end
 
-scope :price_category, -> (price_category_name) do
-    unless price_category_name.blank?
-      if price_category_name == PriceCategories::FREE
-        where(price: 0) 
-      elsif price_category_name == PriceCategories::PREMIUM 
-        where.not(price: 0)
+  scope :price_category, -> (price_category_name) do
+      unless price_category_name.blank?
+        if price_category_name == PriceCategories::FREE
+          where(price: 0) 
+        elsif price_category_name == PriceCategories::PREMIUM 
+          where.not(price: 0)
+        end
       end
     end
-  end
-                                                
 
-scope :industry_category, -> (industry_name) do
-    unless industry_name.blank?
-      where(industry: industry_name) if IndustryCategories::INDUSTRIES_LIST.include?(industry_name)
-    end 
-  end
 
   REQUIRED_DIRECTORIES = %w[assets config layout snippets templates].freeze
   REQUIRED_TEMPLATES = %w[
--- app/models/theme_style.rb
+++ app/models/theme_style.rb
@@ -3,5 +3,19 @@ class ThemeStyle < ApplicationRecord
   belongs_to :theme
 
   validates :name, uniqueness: { scope: :theme_id }
+
+  module IndustryCategories
+    INDUSTRIES_LIST = ["Art & Photography", "Clothing & Fashion",
+                       "Electronics", "Food & Drink",
+                       "Health & Beauty", "Home & Garden",
+                       "Jewelry & Accessories", "Other",
+                       "Responsive", "Sports & Recreation", "Toys & Games" ].freeze
+  end
+
+  scope :industry_category, -> (industry_name) do
+      unless industry_name.blank?
+        where(industry: industry_name) if IndustryCategories::INDUSTRIES_LIST.include?(industry_name)
+      end 
+    end
   
 end
