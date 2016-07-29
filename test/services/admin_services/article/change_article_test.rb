603db546d2222a9ffd128893da251d52a1a5cb72
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2016-07-29 17:52:34 +0300

    change article

--- test/services/admin_services/article/change_article_test.rb
+++ test/services/admin_services/article/change_article_test.rb
@@ -7,7 +7,7 @@ class AdminServices::Article::ChangeArticleTest < ActiveSupport::TestCase
     @blog = create(:blog, shop: @shop)
     @second_blog = create(:blog, shop: @shop)
     @another_article = create(:article, blog: @second_blog, handle: "myhandle", title: "mytitle")
-    @article = create(:article, blog: @blogm)
+    @article = create(:article, blog: @blog)
     @article2 = create(:article, blog: @blog, handle: "Unique handle")
     @success_params = { id: @article.id.to_s, title: "Changed title", blog_id: @blog.id,
                         content: "Changed content", page_title: "Changed page_title", handle: "ChangedC handle",
@@ -18,85 +18,68 @@ class AdminServices::Article::ChangeArticleTest < ActiveSupport::TestCase
   end
 
   test 'should update article with valid params' do
-    service = AdminServices::Article::ChangeArticle.run(@success_params)
-    assert service.valid?
+    service = AdminServices::Article::ChangeArticle
+              .new(blog_id: @blog.id, article_id: @article.id, params: @success_params)
+              .run()
+    assert service.success?
     assert_equal "Changed content", Article.find(@article.id).content
     assert_equal 'Changed title', Article.find(@article.id).title
   end
 
   test 'should not update article with blank_params' do
-    service = AdminServices::Article::ChangeArticle.run(@blank_params)
-    refute service.valid?
-    assert_equal 1, service.article.errors.full_messages.count
-    assert_equal "Blog is not a valid integer", service.article.errors.full_messages.first
+    service = AdminServices::Article::ChangeArticle
+              .new(blog_id: @blog.id, article_id: @article.id, params: @blank_params)
+              .run()
+    refute service.success?
   end
 
   test 'should not update article with existing title' do
+    invalid_params = @success_params.merge({title: @article2.title})
     service = AdminServices::Article::ChangeArticle
-              .run( id:               @article.id.to_s,
-                    title:            @article2.title,
-                    blog_id:          @blog.id,
-                    content:          "Changed content",
-                    page_title:       "Changed page_title",
-                    handle:           "ChangedC handle",
-                    meta_description: "Changed meta_description")
-    refute service.valid?
-    assert_equal 1, service.errors.full_messages.count
-    assert_equal "Title has already been taken", service.errors.full_messages.first
+              .new(blog_id: @blog.id, article_id: @article.id, params: invalid_params)
+              .run
+    refute service.success?
+    assert_equal 1, service.result.errors.full_messages.count
+    assert_equal "Title has already been taken", service.result.errors.full_messages.first
   end
 
   test 'should not update article with existing handle' do
+    invalid_params = @success_params.merge({handle: @article2.handle})
     service = AdminServices::Article::ChangeArticle
-              .run( id:         @article.id.to_s,
-                    title:      "New title",
-                    blog_id:    @blog.id,
-                    content:    "Changed content",
-                    page_title: "Changed page_title",
-                    handle:     @article2.handle,
-                    meta_description: "Changed meta_description")
-    refute service.valid?
-    assert_equal 1, service.article.errors.full_messages.count
-    assert_equal "Handle has already been taken", service.article.errors.full_messages.first
+              .new(blog_id: @blog.id, article_id: @article.id, params: invalid_params)
+              .run
+    refute service.success?
+    assert_equal 1, service.result.errors.full_messages.count
+    assert_equal "Handle has already been taken", service.result.errors.full_messages.first
   end
 
   test 'should not update article with existing handle when blog is changed' do
+    invalid_params = @success_params.merge({handle: @another_article.handle})
     service = AdminServices::Article::ChangeArticle
-              .run( id:         @article.id.to_s,
-                    title:      "New title",
-                    blog_id:    @second_blog.id,
-                    content:    "Changed content",
-                    page_title: "Changed page_title",
-                    handle:     @another_article.handle,
-                    meta_description: "Changed meta_description")
-    refute service.valid?
-    assert_equal 1, service.article.errors.full_messages.count
-    assert_equal "Handle has already been taken", service.article.errors.full_messages.first
+              .new(blog_id: @second_blog.id, article_id: @article.id, params: invalid_params)
+              .run
+    refute service.success?
+    assert_equal 1, service.result.errors.full_messages.count
+    assert_equal "Handle has already been taken", service.result.errors.full_messages.first
   end
 
   test 'should not update article with existing title when blog is changed' do
+    invalid_params = @success_params.merge({title: @another_article.title})
     service = AdminServices::Article::ChangeArticle
-              .run( id:       @article.id.to_s,
-                    title:    @another_article.title,
-                    blog_id:  @second_blog.id,
-                    content:  "Changed content",
-                    page_title: "Changed page_title",
-                    handle:   "handle",
-                    meta_description: "Changed meta_description")
-    refute service.valid?
-    assert_equal 1, service.article.errors.full_messages.count
-    assert_equal "Title has already been taken", service.article.errors.full_messages.first
+              .new(blog_id: @second_blog.id, article_id: @article.id, params: invalid_params)
+              .run
+    refute service.success?
+    assert_equal 1, service.result.errors.full_messages.count
+    assert_equal "Title has already been taken", service.result.errors.full_messages.first
   end
 
   test 'should update article with some blank params' do
+    blank_params = { title: @article.title, content:  "", page_title: "", handle:   "", meta_description: ""}
+
     service = AdminServices::Article::ChangeArticle
-              .run( id:       @article2.id.to_s,
-                    title:    @article2.title,
-                    blog_id:  @blog.id,
-                    content:  "",
-                    page_title: "",
-                    handle:   "",
-                    meta_description: "")
-    assert service.valid?
+              .new(blog_id: @blog.id, article_id: @article.id, params: blank_params)
+              .run
+    assert service.success?
     assert_equal '', service.result.handle
     assert_equal '', service.result.page_title
   end
