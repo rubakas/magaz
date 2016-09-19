4265a25d19cb31077c416df1455eb36e4b9ba23f
Markell Pogosov <szerencsbtlen404@gmail.com>
2016-09-19 22:55:37 +0300

    email_templates without AI

--- app/models/email_template.rb
+++ app/models/email_template.rb
@@ -1,13 +1,15 @@
 class EmailTemplate < ActiveRecord::Base
-  EMAIL_TEMPLATES = %w[ abandoned_checkout_notification 
-                        contact_buyer fulfillment_request 
+  EMAIL_TEMPLATES = %w[ abandoned_checkout_notification
+                        contact_buyer fulfillment_request
                         gift_card_notification
-                        new_order_notification 
-                        new_order_notification_mobile 
+                        new_order_notification
+                        new_order_notification_mobile
                         order_cancelled order_confirmation
-                        refund_notification 
-                        shipping_confirmation 
+                        refund_notification
+                        shipping_confirmation
                         shipping_update ].freeze
 
   belongs_to :shop
+
+  validates :title, :body, presence: true
 end
